adc21a267cd39f3d13c1e7b463399b7fb7a56e8f
sowmik-sec <sowmik.sec@gmail.com>
2024-08-11 21:51:56 +0600

    set http only cookie

--- index.js
+++ index.js
@@ -31,8 +31,16 @@ async function run() {
     // auth related api
     app.post("/jwt", async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, "secret", { expiresIn: "1h" });
-      res.send(token);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+          sameSite: "none",
+        })
+        .send({ success: true });
     });
 
     // services related api
