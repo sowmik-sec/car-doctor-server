1f5d3cca717260dd47f44c92c6caddc556f854f2
sowmik-sec <sowmik.sec@gmail.com>
2024-08-11 22:18:31 +0600

    remove sameSite config

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ async function run() {
         .cookie("token", token, {
           httpOnly: true,
           secure: false,
-          sameSite: "none",
+          // sameSite: "none",
         })
         .send({ success: true });
     });
